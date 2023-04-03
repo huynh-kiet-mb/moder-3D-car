dd10f1236773497c52e21c4d8766187e32db96b5
huynh-kiet-mb <kiet.huynh@muaban.net>
2023-04-03 17:29:26 +0700

    Commit 3/4/2023 3D model

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import './style.css';
 import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
+import { CubeCamera, Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Ground } from './Ground';
+import { Car } from './Car';
+import { Rings } from './Rings';
 
 function CarShow() {
-  return null;
+  return (
+    <>
+      <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
+      <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+      {/* Set up background color with all black */}
+      <color args={[0, 0, 0]} attach={"background"} />
+
+      {/* Set up car model */}
+
+      <CubeCamera resolution={256} frames={Infinity}>
+        {(texture) => (
+          <>
+            <Environment map={texture} />
+            <Car />
+          </>
+        )}
+      </CubeCamera>
+
+
+      {/* Set up ring leon */}
+      <Rings />
+      {/* Set up light (spotlight) in scene */}
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <Ground />
+    </>
+  );
 }
 
 function App() {
